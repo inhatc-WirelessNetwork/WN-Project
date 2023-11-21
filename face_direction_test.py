5ef149ee64f2ca5e604fbef90cc11d2fb463b86a
ninesoo-shot <dlstn9675@naver.com>
2023-11-21 12:32:03 +0900

    face_direction_test.py 정면추가

--- face_direction_test.py
+++ face_direction_test.py
@@ -59,17 +59,14 @@ while cap.isOpened():
 
     if result.multi_face_landmarks:
         for face_landmarks in result.multi_face_landmarks:
-            # 왼쪽 끝점 (가장 왼쪽 얼굴 랜드마크)
             leftmost_point = face_landmarks.landmark[0]
             leftmost_x = int(leftmost_point.x * frame.shape[1])
 
-            # 오른쪽 끝점 (가장 오른쪽 얼굴 랜드마크)
             rightmost_point = face_landmarks.landmark[16]
             rightmost_x = int(rightmost_point.x * frame.shape[1])
 
             results = pose.process(frame)
 
-            # 얼굴이 정면, 왼쪽 측면, 오른쪽 측면인지 구별
             if leftmost_x > frame.shape[1] * 0.6:
                 if results.pose_landmarks:
                     right_left("right")
@@ -81,9 +78,46 @@ while cap.isOpened():
             else:
                 cv2.putText(frame, "FRONT", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow('Face Direction Detection', frame)
+                if results.pose_landmarks:
+                    points = []
+                    for landmark in results.pose_landmarks.landmark:
+                        h, w, _ = frame.shape
+                        x, y = int(landmark.x * w), int(landmark.y * h)
+                        points.append((x, y))
+
+                    for connection in mp_pose.POSE_CONNECTIONS:
+                        partA, partB = connection
+                        cv2.line(frame, points[partA], points[partB], (0, 255, 0), 2)
+
+                    head = mp_pose.PoseLandmark.NOSE.value
+                    left_shoulder = mp_pose.PoseLandmark.LEFT_SHOULDER.value
+                    right_shoulder = mp_pose.PoseLandmark.RIGHT_SHOULDER.value
+                    shoulder_midpoint = ((points[left_shoulder][0] + points[right_shoulder][0]) / 2,
+                                         (points[left_shoulder][1] + points[right_shoulder][1]) / 2)
+
+                    chest_extension_ratio = 0.01
+                    chest = (shoulder_midpoint[0] + chest_extension_ratio * (points[left_shoulder][0] - shoulder_midpoint[0]),
+                             shoulder_midpoint[1] + chest_extension_ratio * (points[left_shoulder][1] - shoulder_midpoint[1]))
+
+                    line_length = ((points[head][0] - chest[0])**2 + (points[head][1] - chest[1])**2)**0.5
+
+                    # 텍스트를 겹치지 않도록 y좌표를 조정
+                    text_y_front = 30
+                    text_y_chest = text_y_front + 40
+                    text_y_turtle_neck = text_y_chest + 40
+
+                    cv2.putText(frame, f'Chest Length: {line_length:.2f}', (10, text_y_chest), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+                    chest = (int(chest[0]), int(chest[1]))
+                    cv2.line(frame, points[head], chest, (0, 255, 0), 2)
+
+                    if line_length < 170 or line_length < -170:
+                        cv2.putText(frame, 'turtle neck', (10, text_y_turtle_neck), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow('Face and Upper Body Pose Estimation', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
