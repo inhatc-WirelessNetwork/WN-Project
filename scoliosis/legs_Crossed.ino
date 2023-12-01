91ffc1d474466c67f8d740c36740f84950cff1de
JSangYun2 <116622873+JSangYun2@users.noreply.github.com>
2023-12-01 15:42:00 +0900

    주석 변경

--- scoliosis/legs_Crossed.ino
+++ scoliosis/legs_Crossed.ino
@@ -23,36 +23,14 @@ void loop() {
   int mfsr_r18d = map(SensorReading4, 0, 1024, 0, 4095);
   int mfsr_r18e = map(SensorReading5, 0, 1024, 0, 4095);
   int mfsr_r18f = map(SensorReading6, 0, 1024, 0, 4095);
+  // map 함수로 0~1024까지의 값을 0~4095 값으로 단계를 나눠 매핑
 
-//  Serial.print("1st sensor(대퇴이두 (왼쪽)):");
-//  Serial.println(mfsr_r18a);
-//  Serial.print("2nd sensor(대퇴이두 (오른쪽)):");
-//  Serial.println(mfsr_r18b);
-//  Serial.print("3rd (둔근(왼쪽)):");
-//  Serial.println(mfsr_r18c);
-//  Serial.print("4th sensor(둔근(오른쪽)):");
-//  Serial.println(mfsr_r18d);
-//  Serial.print("5th sensor(기립근(왼쪽)):");
-//  Serial.println(mfsr_r18e);
-//  Serial.print("6th sensor(기립근(오른쪽)):");
-//  Serial.println(mfsr_r18f);
-
-  int result1 = abs(mfsr_r18a - mfsr_r18b);  //result1 150이상 되면 메시지 전송
-  int result2 = mfsr_r18e + mfsr_r18f;   // result2 값이 1000이상 되면 메시지 전송
+  int result1 = abs(mfsr_r18a - mfsr_r18b);  //result1 = 양쪽 대퇴 이두쪽에 설치된 압력센서값의 차
+  int result2 = mfsr_r18e + mfsr_r18f;   // result2 = 기립근 쪽에 설치된 두 압력센서값의 합
 
   Serial.println(result1);
   Serial.println(result2);
   
-
-  // 둔근에 압력이 감지 될 때 대퇴이두 센서 출력값이 150 이상이면 다리 꼬지 마세요 출력 
-  //if ((abs(SensorReading - SensorReading2) >= 150) && (SensorReading3 != 0) && (SensorReading4 != 0)) {
-   // Serial.println("다리 꼬지 마세요!");
-  //}
-
-  // 기립근에 일정 수준 이상의 압력 감지시 허리 피세요! 출력
-  //if ((SensorReading5 >= 40) && (SensorReading6 >= 40)) {
-    //Serial.println("허리 피세요!");
-  //}
 
   delay(500);
 }
